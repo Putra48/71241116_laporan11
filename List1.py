3e960e7d04d105c5b134b88f596ebca4454e8825
Putra48 <putra.setiawanbiak@gmail.com>
2025-05-09 08:53:19 -0700

    Update List1.py

--- List1.py
+++ List1.py
@@ -1,34 +1,34 @@
 # Latihan 1
-# def tiga_angka_terbaik():
-#     data = input("Masukkan beberapa angka, pisahkan dengan spasi: ")
-#     angka_list = list(map(int, data.split()))
-#     urutan = sorted(angka_list, reverse=True)
-#     tiga_tertinggi = urutan[:3]
-#     print("3 bilangan terbaik:", tiga_tertinggi)
+def tiga_angka_terbaik():
+    data = input("Masukkan beberapa angka, pisahkan dengan spasi: ")
+    angka_list = list(map(int, data.split()))
+    urutan = sorted(angka_list, reverse=True)
+    tiga_tertinggi = urutan[:3]
+    print("3 bilangan terbaik:", tiga_tertinggi)
 
-# tiga_angka_terbaik()
+tiga_angka_terbaik()
 
 # Latihan 2
-# def hitung_rata_rata():
-#     angka_list = []
+def hitung_rata_rata():
+    angka_list = []
 
-#     while True:
-#         data = input("Masukkan angka (atau ketik 'done' untuk selesai): ")
-#         if data.lower() == "done":
-#             break
-#         try:
-#             angka = float(data)
-#             angka_list.append(angka)
-#         except ValueError:
-#             print("Input tidak valid. Masukkan angka atau 'done'.")
+    while True:
+        data = input("Masukkan angka (atau ketik 'done' untuk selesai): ")
+        if data.lower() == "done":
+            break
+        try:
+            angka = float(data)
+            angka_list.append(angka)
+        except ValueError:
+            print("Input tidak valid. Masukkan angka atau 'done'.")
 
-#     if angka_list:
-#         rata_rata = sum(angka_list) / len(angka_list)
-#         print("Rata-rata dari bilangan yang dimasukkan adalah:", rata_rata)
-#     else:
-#         print("Tidak ada angka yang dimasukkan.")
+    if angka_list:
+        rata_rata = sum(angka_list) / len(angka_list)
+        print("Rata-rata dari bilangan yang dimasukkan adalah:", rata_rata)
+    else:
+        print("Tidak ada angka yang dimasukkan.")
 
-# hitung_rata_rata()
+hitung_rata_rata()
 
 # Latihan 3
 def tampilkan_kata_unik(file):
